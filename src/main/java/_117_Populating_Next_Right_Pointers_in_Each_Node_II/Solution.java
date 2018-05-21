eb2cf7e818b7be961b693b50d60993a3170fa76f
Pavel Belevich <belevichp@gmail.com>
2018-05-21 19:50:44 -0400

    117. Populating Next Right Pointers in Each Node II

---
+++ src/main/java/_117_Populating_Next_Right_Pointers_in_Each_Node_II/Solution.java
@@ -0,0 +1,32 @@
+package _117_Populating_Next_Right_Pointers_in_Each_Node_II;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Pavel Belevich
+ */
+public class Solution {
+
+    public void connect(TreeLinkNode root) {
+        List<TreeLinkNode> curr = new ArrayList<>();
+        if (root != null) {
+            curr.add(root);
+        }
+        while (!curr.isEmpty()) {
+            List<TreeLinkNode> next = new ArrayList<>();
+            TreeLinkNode prev = null;
+            for (int i = 0; i < curr.size(); i++) {
+                TreeLinkNode node = curr.get(i);
+                if (prev != null) {
+                    prev.next = node;
+                }
+                prev = node;
+                if (node.left != null) next.add(node.left);
+                if (node.right != null) next.add(node.right);
+            }
+            curr = next;
+        }
+    }
+
+}
---
+++ src/main/java/_117_Populating_Next_Right_Pointers_in_Each_Node_II/TreeLinkNode.java
@@ -0,0 +1,13 @@
+package _117_Populating_Next_Right_Pointers_in_Each_Node_II;
+
+/**
+ * @author Pavel Belevich
+ */
+public class TreeLinkNode {
+    int val;
+    TreeLinkNode left, right, next;
+
+    TreeLinkNode(int x) {
+        val = x;
+    }
+}
